e80b3cc469b35a5a38c412169b6fe40eb4fe8a4a
Tarık Çekirge <tcekirge@gmail.com>
2025-04-04 20:46:23 +0300

    Configuring Query Behaviors - Cache & Stale Data

---
+++ src/util/http.js
@@ -0,0 +1,15 @@
+export const fetchEvents = async () => {
+  //   await new Promise((resolve) => setTimeout(resolve, 2000));
+  const response = await fetch("http://localhost:3000/events");
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching the events");
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { events } = await response.json();
+
+  return events;
+};
